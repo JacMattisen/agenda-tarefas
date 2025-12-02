5449fad663ae2f01e812b1789d342845ac786671
JacMattisen <jjm_jac@yahoo.com.br>
2025-12-02 11:23:12 +0100

    feat: implement reactive form for user registration with full name, email, and password fields

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -6,6 +6,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { PasswordFieldComponent } from '../../shared/components/password-field/password-field.component';
 import { MatIcon } from '@angular/material/icon';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -18,8 +24,27 @@ import { MatIcon } from '@angular/material/icon';
     MatFormFieldModule,
     MatIcon,
     PasswordFieldComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      fullName: [''],
+      email: [''],
+      password: [''],
+    });
+  }
+
+  get passwordControl(): FormControl {
+    return this.form.get('password') as FormControl;
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+}
