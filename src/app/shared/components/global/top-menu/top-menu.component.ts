282c9b988c1b1c525f62266db53f2d459799fe64
JacMattisen <jjm_jac@yahoo.com.br>
2025-12-09 11:42:09 +0100

    disable button register by route

--- src/app/shared/components/global/top-menu/top-menu.component.ts
+++ src/app/shared/components/global/top-menu/top-menu.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-top-menu',
   standalone: true,
   imports: [MatButtonModule, MatIconModule, MatToolbarModule, RouterLink],
   templateUrl: './top-menu.component.html',
-  styleUrl: './top-menu.component.scss',
+  styleUrls: ['./top-menu.component.scss'],
 })
-export class TopMenuComponent {
+export class TopMenuComponent implements OnInit, OnDestroy {
   appLogo = 'assets/logo.jpg';
+
+  rotaAtual: string = '';
+  inscricaoRota!: Subscription;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.rotaAtual = this.router.url;
+
+    this.inscricaoRota = this.router.events
+      .pipe(
+        filter(
+          (event): event is NavigationEnd => event instanceof NavigationEnd
+        )
+      )
+      .subscribe((event) => {
+        this.rotaAtual = event.urlAfterRedirects;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.inscricaoRota?.unsubscribe();
+  }
+
+  isRegisterPage(): boolean {
+    return this.rotaAtual.includes('/register');
+  }
 }
